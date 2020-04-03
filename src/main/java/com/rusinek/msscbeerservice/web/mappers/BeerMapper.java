3617a38543cf4c23fc3955ae1f7d8dd645ae95d6
AdRusinek <rusinekadrian19@gmail.com>
2020-04-04 01:50:54 +0200

    adding CircleCi config

---
+++ src/main/java/com/rusinek/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,15 @@
+package com.rusinek.msscbeerservice.web.mappers;
+
+import com.rusinek.msscbeerservice.domain.Beer;
+import com.rusinek.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by Adrian Rusinek on 04.04.2020
+ **/
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
