897a5bb7ff4e29ba3ade39b9472f834db9cb5afc
AdRusinek <rusinekadrian19@gmail.com>
2020-04-06 02:28:10 +0200

    config projects to use mysql

--- src/main/java/com/rusinek/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/rusinek/msscbeerservice/bootstrap/BeerLoader.java
@@ -8,12 +8,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by Adrian Rusinek on 03.04.2020
  **/
 @RequiredArgsConstructor
-//@Component
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
