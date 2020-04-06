ae8eb67888206c68a597a12f43c767e01cb1e108
AdRusinek <rusinekadrian19@gmail.com>
2020-04-06 22:09:54 +0200

    adding jms config

---
+++ src/main/java/com/rusinek/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package com.rusinek.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by Adrian Rusinek on 06.04.2020
+ **/
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
